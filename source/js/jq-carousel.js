fb8ba1ba41a65b18ec8d9a3da5498cb0c905aa4f
ilebedev-ru <wapstyle@bk.ru>
2018-11-06 00:04:52 +0500

    сделана карусель

--- source/js/jq-carousel.js
+++ source/js/jq-carousel.js
@@ -1,1 +1,72 @@
 'use scrict';
+
+(function() {
+	var serviceCarouselArea = document.querySelector('.service__carousel-area');
+	var serviceCarouselList = serviceCarouselArea.querySelector('.service__list');
+	var serviceBtnPrev = document.querySelector('.service__button--left');
+	var serviceBtnNext = document.querySelector('.service__button--right'); 
+
+	var serviceItems = serviceCarouselArea.querySelectorAll('.service__item');
+	
+	var carouselWindowWidth = serviceCarouselArea.offsetWidth;
+
+	var carouselAllWidth = 0;
+	Array.prototype.forEach.call(serviceItems, function(items) {
+		carouselAllWidth += items.offsetWidth + Number(getComputedStyle(items).marginRight.slice(0, -2));
+	});
+
+	var carouselItemWidth = carouselAllWidth / serviceItems.length + 5;
+
+	currentPositionLeft = -carouselItemWidth;
+	serviceCarouselList.style.left = currentPositionLeft + 'px';
+
+
+	var changePosition = function(current, num) {
+		if (current < num) {
+			var moveCarousel = setInterval(function() {
+ 				current += 20;
+ 				serviceCarouselList.style.left = current + 'px';
+
+ 				if (current === num) {
+ 					clearInterval(moveCarousel)
+ 				}
+ 			}, 10);
+		} else {
+			var moveCarousel = setInterval(function() {
+ 				current -= 20;
+ 				serviceCarouselList.style.left = current + 'px';
+
+ 				if (current === num) {
+ 					clearInterval(moveCarousel)
+ 				}
+ 			}, 10);
+		}
+	};
+
+	serviceBtnPrev.addEventListener('click', function() {
+		changePosition(currentPositionLeft, currentPositionLeft + carouselItemWidth);
+		currentPositionLeft += carouselItemWidth;
+
+		if (currentPositionLeft === 0) {
+			serviceBtnPrev.disabled = true;
+			serviceBtnPrev.style.opacity = 0.2;
+		}
+
+		serviceBtnNext.disabled = false
+		serviceBtnNext.style.opacity = 1;
+	});
+
+
+	serviceBtnNext.addEventListener('click', function() {
+		changePosition(currentPositionLeft, currentPositionLeft + -carouselItemWidth);
+		currentPositionLeft += -carouselItemWidth;
+
+		if (currentPositionLeft + -carouselWindowWidth == -carouselAllWidth) {
+			serviceBtnNext.disabled = true;
+			serviceBtnNext.style.opacity = 0.2;
+		}
+
+		serviceBtnPrev.disabled = false;
+		serviceBtnPrev.style.opacity = 1;
+	});
+})();
